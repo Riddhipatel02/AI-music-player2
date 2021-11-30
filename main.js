ca72e1438bf4fc5a844fdf58bc1cff443e79eae4
Riddhipatel02 <74219262+Riddhipatel02@users.noreply.github.com>
2021-11-29 19:53:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+song1="";
+song2="";
+song1_status="";
+song2_status="";
+
+function preload(){
+    song1=loadSound("music.mp3");
+    song2=loadSound("music2.mp3");
+}
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,600,500);
+    song1_status=song1.isPlaying();
+    song2_status=song2.isPlaying();
+}
+function play(){
+  song1.play();  
+  song.setVolume(1);
+  song.rate(1);
+}
